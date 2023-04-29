5a5b95b94d8c9077380124e1d4bb867048e39c1c
atomicapple <brianzhangaa@gmail.com>
2023-04-29 10:34:45 -0400

    templates

---
+++ template.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define DEBUG 1
+#define log(fmt,...) do { if (DEBUG) { printf("[%s():%d] " fmt "\n", __FUNCTION__, __LINE__, ##__VA_ARGS__); } } while (0)
+template <class T>
+void print_v(vector<T> &v) { if (DEBUG) { cout << "{"; for (auto x : v) cout << x << ","; cout << "\b}"; } }
+typedef vector<int> vi;
+typedef vector<tuple<int,int>> vii;
+typedef vector<vi> vvi;
+typedef map<int,int> mii;
+
+
+int T;
+
+void solve() {
+    printf("solution");
+    return;
+}
+
+int main() {
+    scanf("%d", &T);
+    while (T--) {
+        solve();
+    }
+    return 0;
+}
