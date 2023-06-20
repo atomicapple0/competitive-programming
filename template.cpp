d13bdefebff4a36fc741517937c17c7e9d614590
atomicapple0 <brianzhangaa@gmail.com>
2023-06-20 13:30:05 -0400

    update template

--- template.cpp
+++ template.cpp
@@ -5,13 +5,17 @@ using namespace std;
 #define log(fmt,...) do { if (DEBUG) { printf("[%s():%d] " fmt "\n", __FUNCTION__, __LINE__, ##__VA_ARGS__); } } while (0)
 template <class T>
 void print_v(vector<T> &v) { if (DEBUG) { cout << "{"; for (auto x : v) cout << x << ","; cout << "\b}"; } }
-const int INF=2e5+10;
+
+#define int long long
+#define INF 5e6
+
 typedef vector<int> vi;
 typedef vector<tuple<int,int>> vii;
 typedef vector<vi> vvi;
 typedef map<int,int> mii;
 
-
+// vi A(big, 0);
+// vvi G(big);
 int T;
 
 void solve() {
@@ -19,10 +23,10 @@ void solve() {
     return;
 }
 
-int main() {
+signed main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    scanf("%d", &T);
+    scanf("%lld", &T);
     while (T--) {
         solve();
     }
